35bd37f439715ad1e4442324f30d8cc3d12431ab
jwyant <john.wyant@gmail.com>
2014-07-14 18:00:07 -0400

    Update rtdb.sh
    Got rid of useless case statement in wrapper and just pass the first arg to the python script.

--- rtdb.sh
+++ rtdb.sh
@@ -2,9 +2,6 @@
 
 export HOME=/home/username
 export PYTHONPATH=$HOME/lib/pyroscope/pyrocore/src:$HOME/lib/pyroscope/pyrobase/src
-case $1 in 
-	collect )
-		/usr/bin/env python2 $HOME/src/rtdb/rtdb.py collect;;
-	update )
-		/usr/bin/env python2 $HOME/src/rtdb/rtdb.py update;;
-esac
+RTDBPYPATH=$HOME/bin/rtdb.py
+
+/usr/bin/env python2 $RTDBPYPATH $1
